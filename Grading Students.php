22b3e60b1c05466e6e4495c8ec6482d765639ccc
Alex Toga <aotech10@gmail.com>
2020-06-07 20:03:54 -0500

    Grading Students solution

---
+++ Grading Students.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents($grades) {
+    $multipleOf = 5;
+    foreach( $grades as &$grade ){
+        if (  $grade < 38 ){
+            continue;
+        }
+        $nextMultipleOf = (intdiv( $grade , $multipleOf ) + 1 ) * $multipleOf;
+        if ( ($nextMultipleOf - $grade) < 3 ){
+            $grade = $nextMultipleOf;
+        }
+    }
+    return $grades;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$grades_count = intval(trim(fgets(STDIN)));
+
+$grades = array();
+
+for ($i = 0; $i < $grades_count; $i++) {
+    $grades_item = intval(trim(fgets(STDIN)));
+    $grades[] = $grades_item;
+}
+
+$result = gradingStudents($grades);
+
+fwrite($fptr, implode("\n", $result) . "\n");
+
+fclose($fptr);
